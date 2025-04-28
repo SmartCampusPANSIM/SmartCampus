d4a5b627e2c788f5cbc4fc8a22688a765830ad60
SmartCampusPANSIM <smartcampuspansim@gmail.com>
2025-04-29 00:37:46 +0200

    com2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import './index.css';
-import './SmartCampusUI.css';
+import './smartCampusUI.css';
 import App from './App.jsx';
 import WelcomePage from './WelcomePage.jsx';
 import Elearning from './Elearning.jsx';
