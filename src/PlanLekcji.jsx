ef40c4d342ad67e6149461ffb56b53add007e302
Michal Majewski <majesxddd@gmail.com>
2025-04-29 23:20:46 +0200

    Skończony szablon górnej części. Zaczęte prace nad rozwijaniem list.

--- src/PlanLekcji.jsx
+++ src/PlanLekcji.jsx
@@ -1,20 +1,41 @@
 import { Link } from "react-router-dom";
 import './PlanLekcji.css';
+import React, { useState } from 'react';
 
 function PlanLekcji() {
+
+    const [isActive1, setIsActive1] = useState(false);
+    const expandListKierunek = () => {
+      setIsActive1(prev => !prev);
+    }
+
+    const [isActive2, setIsActive2] = useState(false);
+    const expandListCos = () => {
+      setIsActive2(prev => !prev);
+    }
   return (
     <app className="majes_plan_lekcji">
       <header>
+
         <section className="majes_header_choose__div">
           <p>Wyświetl plan dla:</p>
-          <div className="majes_header_choose">
+          <div className="majes_header_choose" onClick={expandListKierunek}>
           </div>
+          {isActive1 && (
+          <div className="majes_header_list__div">
+          </div>
+          )}
         </section>
+
 
         <section className="majes_header_choose__div">
           <p>Wybierz zjazd</p>
-          <div className="majes_header_choose">
+          <div className="majes_header_choose" onClick={expandListCos}>
           </div>
+          {isActive2 && (
+          <div className="majes_header_list__div">
+          </div>
+          )}
         </section>
 
         <section className="majes_header_choose__div">
