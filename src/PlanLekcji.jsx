bcb1924ccba8e5a87118ec6dc6e82dfd8679ee99
MajesXD <majesxddd@gmail.com>
2025-04-29 20:47:06 +0200

    Rozpoczęte prace nad planem lekcji

--- src/PlanLekcji.jsx
+++ src/PlanLekcji.jsx
@@ -1,10 +1,57 @@
 import { Link } from "react-router-dom";
+import './PlanLekcji.css';
 
 function PlanLekcji() {
   return (
-    <app class="app">
-      <h1>Plan lekcji</h1>
-      {/* Kod planu leckji */}
+    <app className="majes_plan_lekcji">
+      <header>
+        <section className="majes_header_choose__div">
+          <p>Wyświetl plan dla:</p>
+          <div className="majes_header_choose">
+          </div>
+        </section>
+
+        <section className="majes_header_choose__div">
+          <p>Wybierz zjazd</p>
+          <div className="majes_header_choose">
+          </div>
+        </section>
+
+        <section className="majes_header_choose__div">
+          <p>Wybierz miesiąc</p>
+          <div className="majes_header_choose">
+          </div>
+        </section>
+
+        <section className="majes_header_choose_day__div">
+          <p>Wybierz dzień</p>
+          <div className="majes_header_choose_day__days">
+            <img src="./src/assets/arrowLeft-blue.svg"></img>
+            <div className="majes_header_choose_day"></div>
+            <div className="majes_header_choose_day"></div>
+            <div className="majes_header_choose_day"></div>
+            <div className="majes_header_choose_day"></div>
+            <div className="majes_header_choose_day"></div>
+            <div className="majes_header_choose_day"></div>
+            <div className="majes_header_choose_day"></div>
+
+            <img src="./src/assets/arrowRight-blue.svg"></img>
+
+
+
+          </div>
+        </section>
+
+      </header>
+
+      <main class="majes_main">
+        <section className="majes_main_top">
+        <p>Zajęcia w dniu [data]</p>
+        </section>
+        <section className="majes_main_bottom">
+
+        </section>
+      </main>
     </app>
   );
 }
