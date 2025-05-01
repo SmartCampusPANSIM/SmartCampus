103e8b738aa395f2a5e3c113b3ec805583b2ea34
SmartCampusPANSIM <smartcampuspansim@gmail.com>
2025-05-01 23:58:00 +0200

    Wibracja na przyciskach mobile nav bar

--- src/MobileNavBar.jsx
+++ src/MobileNavBar.jsx
@@ -1,14 +1,29 @@
-import { Link, NavLink } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 
 function MobileNavBar() {
+  const handleVibration = () => {
+    if ("vibrate" in navigator) {
+      navigator.vibrate(30); 
+    }
+  };
+
   return (
     <div className="navbar_navigationMobileBar">
-      <NavLink to="/Panel" className={({ isActive }) => isActive ? "navbar_button--active" : "navbar_button"}><i class="fa-solid fa-table-columns"></i></NavLink>
-      <NavLink to="/PlanLekcji" className={({ isActive }) => isActive ? "navbar_button--active" : "navbar_button"}><i class="fa-solid fa-table-list"></i></NavLink>
-      <NavLink to="/MapaKampusu" className={({ isActive }) => isActive ? "navbar_button--active" : "navbar_button"}><i class="fa-solid fa-map-location-dot"></i></NavLink>
-      <NavLink to="/Elearning" className={({ isActive }) => isActive ? "navbar_button--active" : "navbar_button"}><i class="fa-solid fa-clapperboard"></i></NavLink>
-      <NavLink to="/Feed" className={({ isActive }) => isActive ? "navbar_button--active" : "navbar_button"}><i class="fa-solid fa-users-line"></i></NavLink>
+      <NavLink to="/Panel" onClick={handleVibration} className={({ isActive }) => isActive ? "navbar_button--active" : "navbar_button"}>
+        <i className="fa-solid fa-table-columns"></i>
+      </NavLink>
+      <NavLink to="/PlanLekcji" onClick={handleVibration} className={({ isActive }) => isActive ? "navbar_button--active" : "navbar_button"}>
+        <i className="fa-solid fa-table-list"></i>
+      </NavLink>
+      <NavLink to="/MapaKampusu" onClick={handleVibration} className={({ isActive }) => isActive ? "navbar_button--active" : "navbar_button"}>
+        <i className="fa-solid fa-map-location-dot"></i>
+      </NavLink>
+      <NavLink to="/Elearning" onClick={handleVibration} className={({ isActive }) => isActive ? "navbar_button--active" : "navbar_button"}>
+        <i className="fa-solid fa-clapperboard"></i>
+      </NavLink>
+      <NavLink to="/Feed" onClick={handleVibration} className={({ isActive }) => isActive ? "navbar_button--active" : "navbar_button"}>
+        <i className="fa-solid fa-users-line"></i>
+      </NavLink>
     </div>
   );
 }
