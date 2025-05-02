579dbb37b14855521064b10f64918e534f7ec4c0
Michal Majewski <majesxddd@gmail.com>
2025-05-02 23:22:21 +0200

    Listy rozwijane się rozwijają i zwijają.

--- src/PlanLekcji.jsx
+++ src/PlanLekcji.jsx
@@ -7,41 +7,46 @@ function PlanLekcji() {
   // Wybór kierunku
   const [Active1, setActive1] = useState(false);
   const kierunekRef = useRef(null);
-  const expandListKierunek = () => {
-    setActive1(prev => !prev);
-  };
+  const kierunekButtonRef = useRef(null);
 
   // Wybór zjazdu
   const [Active2, setActive2] = useState(false);
   const zjazdRef = useRef(null);
-  const expandListZjazd = () => { 
-    setActive2(prev => !prev);
-  };
+  const zjazdButtonRef = useRef(null);
+
   // Wybór miesiąca
   const [Active3, setActive3] = useState(false);
   const miesiacRef = useRef(null);
-  const expandListMiesiac = () => {
-    setActive3(prev => !prev);
-  };
+  const miesiacButtonRef = useRef(null);
 
+// Znikanie listy przy kliknięciu poza nią
   useEffect(() => {
-    function handleClickOutside(event) {
-      if (kierunekRef.current && !kierunekRef.current.contains(event.target)) {
+    function clickOutside(event) {
+      // Dla kierunku
+      const outsideKierunekList = kierunekRef.current && !kierunekRef.current.contains(event.target);
+      const outsideKierunekButton = kierunekButtonRef.current && !kierunekButtonRef.current.contains(event.target);
+      if (Active1 && outsideKierunekList && outsideKierunekButton) {
         setActive1(false);
       }
-      if (zjazdRef.current && !zjazdRef.current.contains(event.target)) {
+      //  Dla zjazdu
+      const outsideZjazdList = zjazdRef.current && !zjazdRef.current.contains(event.target);
+      const outsideZjazdButton = zjazdButtonRef.current && !zjazdButtonRef.current.contains(event.target);
+      if (Active2 && outsideZjazdList && outsideZjazdButton) {
         setActive2(false);
       }
-      if (miesiacRef.current && !miesiacRef.current.contains(event.target)) {
+      // Dla miesiąca
+      const outsideMiesiacList = miesiacRef.current && !miesiacRef.current.contains(event.target);
+      const outsideMiesiacButton = miesiacButtonRef.current && !miesiacButtonRef.current.contains(event.target);
+      if (Active3 && outsideMiesiacList && outsideMiesiacButton) {
         setActive3(false);
       }
     }
 
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", clickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", clickOutside);
     };
-  }, []);
+  }, [Active1, Active2, Active3]);
 
 
   const alercik = () => {
@@ -54,7 +59,7 @@ function PlanLekcji() {
 
         <section className="majes_header_choose__div">
           <p>Wyświetl plan dla:</p>
-          <div className="majes_header_choose" onClick={expandListKierunek}>
+          <div className="majes_header_choose" onClick={() => setActive1(prev => !prev)} ref={kierunekButtonRef}>
             <div className="majes_header_choose-info"></div>
             <img src="./src/assets/arrow_down-white.svg"></img>
           </div>
@@ -70,7 +75,7 @@ function PlanLekcji() {
 
         <section className="majes_header_choose__div">
           <p>Wybierz zjazd</p>
-          <div className="majes_header_choose" onClick={expandListZjazd}>
+          <div className="majes_header_choose" onClick={() => setActive2(prev => !prev)} ref={zjazdButtonRef}>
           <div className="majes_header_choose-info"></div>
           <img src="./src/assets/arrow_down-white.svg"></img>
           </div>
@@ -85,7 +90,7 @@ function PlanLekcji() {
 
         <section className="majes_header_choose__div">
           <p>Wybierz miesiąc</p>
-          <div className="majes_header_choose" onClick={expandListMiesiac}>
+          <div className="majes_header_choose" onClick={() => setActive3(prev => !prev)} ref={miesiacButtonRef}>
           <div className="majes_header_choose-info"></div>
           <img src="./src/assets/arrow_down-white.svg"></img>
           </div>
