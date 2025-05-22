b36a447fb70a1c3291cebf7d9712ce9d25eeb07c
Michal Majewski <majesxddd@gmail.com>
2025-05-22 23:46:31 +0200

    poprawka top na margin-top

--- src/Panel.jsx
+++ src/Panel.jsx
@@ -1,9 +1,24 @@
 import { Link } from "react-router-dom";
-
+import List from './Lista';
 function Panel() {
   return (
     <app class="app">
       <h1>Panel</h1>
+      <List
+        listButtonTop="2025"
+        listButtonBot="Kwiecień"
+        choices={[
+          { label: 'Maj', value: '' },
+          { label: 'Czerwiec', value: '' },
+          { label: 'Lipiec', value: '' },
+          { label: 'Sierpień', value: '' },
+          { label: 'Wrzesień', value: '' },
+          { label: 'Październik', value: '' },
+          { label: 'Listopad', value: '' },
+          { label: 'Grudzień', value: '' },
+        ]}
+        onSelect={(option) => console.log(option)}
+      />
       <h5>sadasdda</h5>
       <h5>sadasdda</h5>
       <h5>sadasdda</h5>
